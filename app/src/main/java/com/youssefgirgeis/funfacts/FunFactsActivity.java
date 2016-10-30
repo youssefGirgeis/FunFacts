e9f044e08fad04594d9e85b7ea8e14ec756cbef6
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-30 12:58:15 -0400

    add a TAG

--- app/src/main/java/com/youssefgirgeis/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/youssefgirgeis/funfacts/FunFactsActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -13,6 +14,8 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
 
     private TextView mFactTextView;
     private Button mShowFactButton;
@@ -45,6 +48,8 @@ public class FunFactsActivity extends AppCompatActivity {
         };
         mShowFactButton.setOnClickListener(listener);
 
-        Toast.makeText(FunFactsActivity.this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(FunFactsActivity.this, "Yay! Our Activity was created!", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "we are loggind from oncreate() method");
     }
 }
